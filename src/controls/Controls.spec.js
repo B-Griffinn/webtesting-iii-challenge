4d053140f4ecf2a35f0a86c67126654f54746b51
Ben <bigriffin94@outlook.com>
2019-09-16 15:41:36 -0700

    MVP met

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,44 +1,48 @@
 // Test away!
-// what is needed in order to test?
-    // renderer, fireEvent + render, controls Component
-import React from 'react';
-import renderer from 'react-test-renderer';
-import { fireEvent, render } from '@testing-library/react';
-import Controls from './Controls';
-import { get } from 'http';
+import React from "react";
+import renderer from "react-test-renderer";
+import { fireEvent, render } from "@testing-library/react";
+import Controls from "./Controls";
 
-// Create a snapShot
-describe('This snapshot tests the Controls Component', () => {
-    it('SnapShot matches!', () => {
-        const tree = renderer.create(<Controls/>);
-        expect(tree.toJSON()).toMatchSnapshot();
-    });
+describe("Snapshot Testing <Controls />", () => {
+  it("matches Snapshot", () => {
+    const tree = renderer.create(<Controls />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
 
-    it('should invoke a function toggle locked after the button is pressed', () => {
-        const toggleLocked = jest.fn();
-        const locked = true;
-        const closed = true;
-        const { getByText } = render(
-            <Controls toggleLocked={toggleLocked} locked={locked} clsoed={closed}/>
-        );
-        // FireEvent
-        fireEvent.click(getByText('Unlock Gate'));
-        expect(toggleLocked).toHaveBeenCalled();
-    });
-    it('should invoke a function toggle close after button is pressed', () => {
-        const toggleClosed = jest.fn();
-        const locked = false;
-        const closed = false;
-        const { getByText } = render(
-            <Controls toggleClosed={toggleClosed} locked={locked} closed={closed} />
-        );
-        fireEvent.click(getByText('Close Gate'));
-        expect(toggleClosed).toHaveBeenCalled();
-    }); //end fireEvent
+  it("should invoke the function toggle locked after button pressed", () => {
+    const toggleLocked = jest.fn();
+    let locked = true;
+    let closed = true;
 
-    it('Check if the button is disabled', () => {
-        const { getByText } = render(<Controls locked={true} closed={true} />);
-        expect(getByText("Open Gate").disabled).toBeTruthy();
-    })
+    const { getByText } = render(
+      <Controls toggleLocked={toggleLocked} locked={locked} closed={closed} />
+    );
+    fireEvent.click(getByText("Unlock Gate"));
+    expect(toggleLocked).toHaveBeenCalled();
+  });
 
-}); //end of snapshot
+  it("should invoke a function toggle close after button pressed", () => {
+    const toggleClosed = jest.fn();
+    const locked = false;
+    const closed = false;
+
+    const { getByText } = render(
+      <Controls toggleClosed={toggleClosed} locked={locked} closed={closed} />
+    );
+    fireEvent.click(getByText("Close Gate"));
+    expect(toggleClosed).toHaveBeenCalled();
+  });
+
+  it("Checking if the button is disabled", () => {
+    const { getByText } = render(<Controls locked={true} closed={true} />);
+
+    expect(getByText("Open Gate").disabled).toBeTruthy();
+  });
+
+
+  it("Checking the button if disabled", () => {
+    const { getByText } = render(<Controls />);
+    expect(getByText("Lock Gate").disabled).toBeTruthy();
+  });
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,10 @@
-// Test away
+import React from "react";
+import renderer from "react-test-renderer";
+import Dashboard from "./Dashboard";
+
+describe('<Dashboard />', () => {
+  it('matches the snapshot', () => {
+    const tree = renderer.create(<Dashboard />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  })
+})
