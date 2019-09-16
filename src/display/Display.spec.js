c47493fe32cc810db154848d2ac182d69e1ec27e
Ben <bigriffin94@outlook.com>
2019-09-16 15:48:07 -0700

    Tests Updated

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -10,35 +10,3 @@ describe("<Display />", () => {
     expect(tree.toJSON()).toMatchSnapshot();
   });
 
-  it("should Displays unlocked", () => {
-    const { queryByText } = render(<Display />);
-    expect(queryByText(/unlocked/i)).toBeTruthy();
-  });
-
-  it("should Displays open", () => {
-    const { queryByText } = render(<Display />);
-    expect(queryByText(/open/i)).toBeTruthy();
-  });
-
-  it('should "closed" and "locked" equal false', () => {
-    const { queryByText } = render(<Display closed={true} locked={true} />);
-    expect(queryByText(/closed/i)).toBeTruthy();
-    expect(queryByText(/locked/i)).toBeTruthy();
-  });
-
-
-  it("Should have red-led when locked or closed", () => {
-    const { queryAllByTestId } = render(
-      <Display closed={false} locked={false} />
-    );
-    expect(queryAllByTestId("led green-led")).toBeTruthy();
-  });
-
-  it("Should have red-led when locked or closed", () => {
-    const { queryAllByTestId } = render(
-      <Display closed={true} locked={true} />
-    );
-    expect(queryAllByTestId("led red-led")).toBeTruthy();
-  });
-
-});
